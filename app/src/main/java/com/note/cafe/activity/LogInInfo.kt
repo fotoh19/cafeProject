8c7befb0343cf6df59df244cefe02ab371eeb07e
fotoh19 <129291991+fotoh19@users.noreply.github.com>
2024-08-23 20:57:53 +0300

    cafe

--- app/src/main/java/com/note/cafe/activity/LogInInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.note.cafe.activity
-
-class LogInInfo {
-    fun Verify(Username:String,password:String):Boolean{
-        return Username== "fotoh" &&
-                password == "123"
-    }
-}
